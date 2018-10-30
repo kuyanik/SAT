929ec8cce4db46cdf91d87a70b108625da65b435
kuyanik <korcan_uyanik@sfu.ca>
2018-10-30 11:51:24 -0700

    Need to change combination algorithm

--- sat/sat.cpp
+++ sat/sat.cpp
@@ -43,26 +43,29 @@ bool is_all_positive(vector <int> key){
 	}
 	return 1;
 }
-void alter_key(vector<int>& key,int& full_loop){
+void alter_key(vector<int>& key,int& full_loop,int& hold_loop){
 
 	int last_index = key.size() - 1;
 
 	if(is_all_positive(key) == 1){
 		key[0] *= -1;
 	}
-
-	else if(is_all_negative(key) == 1){
+	else if(hold_loop == 1){
 		full_loop = 1;
+	}
+	else if(is_all_negative(key) == 1){
+		key[0] *= -1;
+		hold_loop = 1;
 	}
 
 	else if( key[last_index] != abs( key[last_index] ) ){ // if last item is negative
 		key[last_index] *= -1;
-		int last_negative_index = find_last_negative(key); //finding the last negative item
+		int last_negative_index = find_last_negative(key);
 		key[last_negative_index+1] *= -1;
 		key[last_negative_index+2] *= -1;
 	}
 	else{
-		int last_negative_index = find_last_negative(key); //finding the last negative item
+		int last_negative_index = find_last_negative(key);
 		key[last_negative_index] *= -1;
 		key[last_negative_index+1] *= -1;
 	}
@@ -78,8 +81,7 @@ vector<int> create_key(int variable){
 int main(){
 
 	vector< vector<int> > arr;
-	arr.resize(100);
-
+	arr.resize(1);
 	ifstream infile;
 	infile.open("example.txt");
 	if (!infile){
@@ -97,15 +99,16 @@ int main(){
 		}
 		else if (x == 0){
 			++i;
+			arr.push_back( vector<int> () );
 		}
 	}
 
 	infile.close();
+	int hold_loop = 0;
 	int full_loop = 0;
 	int match = 1;
 	vector<int> key = create_key(4);
-
-	while(full_loop != 1){
+	while(full_loop != 1 && hold_loop != 1){
 		int j = 0;
 		while(j < arr.size()){
 			match = check_match(arr[j],key);
@@ -113,9 +116,10 @@ int main(){
 			if(match == 0)
 				break;
 		}
-		alter_key(key,full_loop);
+		cout<<hold_loop<< full_loop<<endl;
 		if(match == 1)
 			break;
+		alter_key(key,full_loop,hold_loop);
 	}
 	cout<<"writing the key: ";
 	for(int i = 0;i < key.size();i++)
