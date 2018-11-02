e09c9c2b4e7c2954a56c8ccf4813adca38c927b6
kuyanik <korcan_uyanik@sfu.ca>
2018-11-01 23:41:58 -0700

    Re-implementation, vector needs fix

--- sat/sat.cpp
+++ sat/sat.cpp
@@ -16,60 +16,6 @@ bool check_match(vector<int> arr,vector<int> key){
 	return 0;
 }
 
-int find_last_negative(vector<int> key){
-	int i = 0;
-	int index = 0;
-	int size = key.size();
-
-	while(i < size){
-		if( key[i] != abs(key[i]) )
-			index = i;
-		++i;
-	}
-	return index;
-}
-
-bool is_all_negative(vector<int> key){
-	for(int i =0;i<key.size();i++){
-		if(abs(key[i]) == key[i])
-			return 0;
-	}
-	return 1;
-}
-bool is_all_positive(vector <int> key){
-	for(int i =0;i<key.size();i++){
-		if(abs(key[i]) != key[i])
-			return 0;
-	}
-	return 1;
-}
-void alter_key(vector<int>& key,int& full_loop,int& hold_loop){
-
-	int last_index = key.size() - 1;
-
-	if(is_all_positive(key) == 1){
-		key[0] *= -1;
-	}
-	else if(hold_loop == 1){
-		full_loop = 1;
-	}
-	else if(is_all_negative(key) == 1){
-		key[0] *= -1;
-		hold_loop = 1;
-	}
-
-	else if( key[last_index] != abs( key[last_index] ) ){ // if last item is negative
-		key[last_index] *= -1;
-		int last_negative_index = find_last_negative(key);
-		key[last_negative_index+1] *= -1;
-		key[last_negative_index+2] *= -1;
-	}
-	else{
-		int last_negative_index = find_last_negative(key);
-		key[last_negative_index] *= -1;
-		key[last_negative_index+1] *= -1;
-	}
-}
 vector<int> create_key(int variable){
 	vector<int> new_key;
 	for(int i =1; i<=variable ;i++){
@@ -81,7 +27,7 @@ vector<int> create_key(int variable){
 int main(){
 
 	vector< vector<int> > arr;
-	arr.resize(1);
+
 	ifstream infile;
 	infile.open("example.txt");
 	if (!infile){
@@ -91,39 +37,26 @@ int main(){
 
 	int i = 0;
 	int x;
-
+	arr.push_back( vector<int>() );
 	while(infile >> x){
-	cout<<x<<endl;
 		if(x !=0 ){
 			arr[i].push_back(x);
 		}
 		else if (x == 0){
 			++i;
-			arr.push_back( vector<int> () );
+			arr.push_back( vector<int>() );
 		}
 	}
 
 	infile.close();
-	int hold_loop = 0;
-	int full_loop = 0;
-	int match = 1;
-	vector<int> key = create_key(4);
-	while(full_loop != 1 && hold_loop != 1){
-		int j = 0;
-		while(j < arr.size()){
-			match = check_match(arr[j],key);
-			++j;
-			if(match == 0)
-				break;
-		}
-		cout<<hold_loop<< full_loop<<endl;
-		if(match == 1)
-			break;
-		alter_key(key,full_loop,hold_loop);
-	}
-	cout<<"writing the key: ";
-	for(int i = 0;i < key.size();i++)
-	cout<<key[i]<<" ";
 
+/*
+cout<<arr.size()<<"-----\n";
+for(int i = 0 ; i < arr.size() ; ++i){
+	for(int k = 0; k < arr[k].size(); ++k){
+		cout<< arr[i][k] <<endl;
+	}
+}
+*/ cout<<arr[0][3]<<endl;
 	return 0;
 }
